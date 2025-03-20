b4f718caa11f21b77b1aad4fa0c2f8803dcca47a
t-fukuda <fukudatatsuya911@gmail.com>
2025-03-20 11:00:33 +0900

    言語切り替え機能を追加した

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -90,7 +90,8 @@ Cache::config('default', array('engine' => 'File'));
  */
 Configure::write('Dispatcher.filters', array(
 	'AssetDispatcher',
-	'CacheDispatcher'
+	'CacheDispatcher',
+	'LanguageFilter'
 ));
 
 /**
